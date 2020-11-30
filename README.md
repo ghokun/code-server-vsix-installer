69effa4fbdcd1801fef15f1b589e0d0c571c837c
ghokun <gokhun@gmail.com>
2020-11-30 09:40:17 +0300

    Installer script for .vsix files is added.
    Signed-off-by: ghokun <gokhun@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# code-server-vsix-installer
-An extension installer script for code-server
+# Code Server .vsix Installer
+An extension installer script for code-server. Installs extensions from Microsoft Visual Studio Marketplace. Useful for extensions that are not found in code-server's extension listings.
+
+## Usage
+Open up a code-server instance. Inside your code-server open a terminal.
+
+```bash
+git clone https://github.com/ghokun/code-server-vsix-installer.git
+cd code-server-vsix-installer
+# Edit extensions.csv file as your needs, example contains extensios for a java development workspace
+chmod +x installer.sh
+./installer.sh
+```
+
+## TODO
+- [ ] Check for aarch64 compability.
+- [ ] Compare versions by their semver.
---
+++ installer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+DOWNLOAD_DIR=/tmp
+EXTENSIONS_DIR=/config/extensions
+
+while IFS=, read -r publisher extension version;
+do
+EXTENSION_URL="https://$publisher.gallery.vsassets.io/_apis/public/gallery/publisher/$publisher/extension/$extension/$version/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
+INSTALLED_VERSION=$(code-server --extensions-dir /config/extensions --list-extensions --show-versions|grep -i $publisher|sed 's/.*\@//')
+  if [ "$INSTALLED_VERSION" == $version ]; then
+    echo "[ $publisher.$extension@$INSTALLED_VERSION.vsix is already installed ]"
+  else
+    echo "[ Downloading $publisher.$extension@$version.vsix ]"
+    curl -s -o $DOWNLOAD_DIR/$publisher.$extension@$version.vsix $EXTENSION_URL
+    echo "[ Installing $publisher.$extension@$version ]"
+    code-server --extensions-dir $EXTENSIONS_DIR --install-extension $DOWNLOAD_DIR/$publisher.$extension@$version.vsix
+    rm $DOWNLOAD_DIR/$publisher.$extension@$version.vsix
+  fi
+done < <(grep "" ./extensions.csv)
